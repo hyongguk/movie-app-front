4624b34116d12b245a68c4d6095b7e9b804a7b64
Hyong <hyongguk1984@gmail.com>
2020-09-15 23:12:35 +0900

    before challenge

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import useState from "react"
 
-function App() {
-  fetch("http://localhost:4000/").then(result => result.text()).then(res => console.log(res));
+
+export default function App() {
+  const [curImgs, setImg] = useState([]);
+  //let img = ""
+  let arr = [];
+  (async () =>{
+    let img = await fetch("http://localhost:4000/").then(result => result.text()).then(res => arr.push(res));
+    // setImg([...curImgs, img]);
+    // console.log(curImgs)
+  })()
+
+  // async function getPoster(){
+  //   //
+  //   let res = await axios.get("http:localhost:4000/");
+  //   console.log(3)
+  //   let img = res.data;
+  //   console.log(res);
+  // }
+  // getPoster();
   return (
     <div className="App">
       <header className="App-header">
@@ -11,6 +30,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <img src={arr[0]} />
         <a
           className="App-link"
           href="https://reactjs.org"
